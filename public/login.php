2571a8f1347556b5ff22da6f9f039f046d2292e3
Denis Aksenov <daksyonoff@gmail.com>
2025-05-06 18:55:26 +0300

    config upd

--- public/login.php
+++ public/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$pdo = require_once '../config/database.php';
+$pdo = require_once __DIR__ . '/../config/database.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
